f6a360825f0620c48d0351752703ffdf14f77b5f
Cory Shain <cory.shain@gmail.comx>
2025-08-24 08:52:57 -0700

    Added templateflow var to quickstart

--- climbprep/constants.py
+++ climbprep/constants.py
@@ -495,6 +495,7 @@ umask 002
 BASHRC_CLIMBLAB = r'''export APPTAINER_BIND={{LAB_PATH}},/juice6/u/{{USER}},/juice6/scr6/{{USER}},/afs/cs.stanford.edu/u/{{USER}}/{{USER}}
 export APPTAINER_CACHEDIR=/juice6/u/{{USER}}/.apptainer/cache
 export FREESURFER_HOME={{LAB_PATH}}/freesurfer
+export TEMPLATEFLOW_HOME={{LAB_PATH}}/templateflow
 export PATH=$PATH:/u/nlp/bin:/usr/local/cuda:{{APPTAINER_PATH}}/bin:{{CODE_PATH}}/climbprep/bin:{{LAB_PATH}}/bin:{{CODE_PATH}}/flywheel
 export PYTHONPATH=$PYTHONPATH:{{CODE_PATH}}/climbprep:{{CODE_PATH}}/parcellate
 export TMPDIR={{LAB_PATH}}/tmp
